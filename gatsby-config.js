3842fd7d1ef6aed1618468b76897c15e408ec5ad
Andy <andy.speirs2@gmail.com>
2020-06-18 22:46:34 +0100

    Removed image path from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,13 +23,13 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     name: 'images',
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     'gatsby-transformer-sharp',
   ],
 };
