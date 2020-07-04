a71efcf1bfa1621d73cbd4db6001410dc13965e1
Andy <andy.speirs2@gmail.com>
2020-07-04 16:26:11 +0100

    Added version number to header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,40 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
 import styles from '../styles/layout.module.scss';
 import '../styles/layout.scss';
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(
-    graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-            version
-          }
-        }
-      }
-    `,
-  );
+const Layout = ({ children }) => (
+  <div className={styles.root}>
+    <Header />
 
-  const { title, version } = data.site.siteMetadata;
-
-  return (
-    <div className={styles.root}>
-      <Header siteTitle={title} />
-
-      <main className={styles.scroller}>
-        {children}
-      </main>
-      <span className={styles.visuallyHidden}>
-        {version}
-      </span>
-    </div>
-  );
-};
+    <main className={styles.scroller}>
+      {children}
+    </main>
+  </div>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
